b11c9123bc093787217c4eeb2e148810d2fe4b21
Nikita <nickvasky@gmail.com>
2023-07-03 16:12:01 +0300

    Example to get RosTelecom coverage maps using Yandex Maps tiles. It also uses OpenCVSharp to measure coverage from downloaded coverage map.

---
+++ Analyze.cs
@@ -0,0 +1,19 @@
+using OpenCvSharp;
+
+public class CoverageBitmap
+{
+    public static double GetCoveragePercentage(string? fileName)
+    {
+        if (String.IsNullOrEmpty(fileName))
+        {
+            Console.WriteLine($"No coverage map file. Coverage is 0%");
+            return 0.0;
+        }
+        Console.WriteLine($"Calculating coverage percentage...");
+        Mat src = new Mat(fileName, ImreadModes.Grayscale);
+        int totalPixels = src.Rows * src.Cols;
+        int numOfColoredPixels = src.CountNonZero();
+        src.Dispose();
+        return (double)numOfColoredPixels/(double)totalPixels;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double lat = 55.7805983; //y
+        double lon = 49.09003; //x
+        int zoom = 11;
+        string dir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+        string? savedFile = Yandex.Helper.GetCoverageMap(lat, lon, zoom, "2g", dir);
+        double result = CoverageBitmap.GetCoveragePercentage(savedFile);
+        Console.WriteLine($"Coverage is {result*100:F3}%");
+    }
+
+}
